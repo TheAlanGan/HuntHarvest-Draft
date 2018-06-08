2bc38da611c3b31dfa6c3dc581c4a1d0ee50b852
Alan <alan.gan@outlook.com>
2018-06-08 14:45:42 -0400

    Un-hardcoded Parameters and Changed Parameter Values
    Moved the setting of parameters to the top of code rather than hardcoded
    within lines. Also changed the way steepness (k) was calculated for
    logistic curves.

--- agouti-brazilnut.R
+++ agouti-brazilnut.R
@@ -4,18 +4,28 @@
 ###=======================================================================
 highHarvestFecundity <- 0.85 # Multiplier for fecundity rate for Adult trees
 highHarvestSurvival <- 0.9 # Multiplier for survival rate of Adult trees
+agoutiGrowth <- 0.18 # Growth rate of Agoutis in logistic model
+
 lowHunting <- 0.1 # Percentage of agoutis hunted during LOW hunting
 highHunting <- 0.25 # Percentage of agoutis hunted during HIGH hunting
-agoutiGrowth <- 0.5 # Growth rate of Agoutis in logistic model
 
-seedling_init <- 5000 #Initial Populations
-sapling_init <- 500
-adult_init <- 100
-agouti_init <- 5200
+seedlingInit <- 5000 # Initial Populations
+saplingInit <- 500
+adultInit <- 100
+agoutiInit <- 5200
 
-time_end <- 100 # Length of simulation in years
+seedlingCapacity <- 5000 # Carrying Capacities
+saplingCapacity <- 500
+adultCapacity <- 100
+agoutiCapacity <- 5200
+
+agouti_to_PlantSteepness <- -(log(1-0.05)-log(0.05))/((0.05-0.5)*agoutiCapacity)
+plant_to_AgoutiSteepness <- -(log(1-0.05)-log(0.05))/((0.05-0.5)*adultCapacity)
+
+time_end <- 1000 # Length of simulation in years
 
 #=========================================================================
+
 
 
 ###========================================================================
@@ -39,6 +49,7 @@ high_harv[cbind(12:17,12:17)] <- highHarvestSurvival # Multiplier for survival r
 # Plant
 plant_mat_low <- plant_S_mat
 plant_mat_high <- plant_S_mat * high_harv
+
 sigmoid <- function(k, x0, x) 
 {
   1/(1+exp(-k*(x-x0))) #k: steepness #x0 = midpoint
@@ -75,12 +86,12 @@ head(harvest_seq)
 ###====================================================================
 plant_mat <- matrix(0, nrow = 17)
 
-plant_mat[1:4] <- seedling_init/4   #Setting initial population of seedlings
-plant_mat[5:11] <- sapling_init/7   #Setting initial population of saplings
-plant_mat[12:17] <- adult_init/6  #Setting initial population of adult trees
-agouti_vec <- c(agouti_init) # Initializing the vector containing agouti pop at each timestep
+plant_mat[1:4] <- seedlingInit/4   #Setting initial population of seedlings
+plant_mat[5:11] <- saplingInit/7   #Setting initial population of saplings
+plant_mat[12:17] <- adultInit/6  #Setting initial population of adult trees
+agouti_vec <- c(agoutiInit) # Initializing the vector containing agouti pop at each timestep
 
-plant_all <- matrix( c(seedling_init, sapling_init, adult_init) ) # This will contain the summed plant populations at ALL timesteps
+plant_all <- matrix( c(seedlingInit, saplingInit, adultInit) ) # This will contain the summed plant populations at ALL timesteps
 
 
 for (i in 1:time_end) 
@@ -95,16 +106,14 @@ for (i in 1:time_end)
   
   else 
   {
-
     pmat <- plant_mat_high
     h_off <- highHunting
-
   }
   
-  p <- sigmoid(0.1, 50, sum(plant_mat[12:17]))*.1 + 0.9 # bounded between 0.9 and 1.0
-  agouti_vec[(i+1)] <- LogisticGrowthHunt(agoutiGrowth, agouti_vec[(i)],5200,h_off, p)
+  p <- sigmoid(plant_to_AgoutiSteepness, 50, sum(plant_mat[12:17]))*.1 + 0.9 # bounded between 0.9 and 1.0.... k was 0.1
+  agouti_vec[(i+1)] <- LogisticGrowthHunt(agoutiGrowth, agouti_vec[(i)],agoutiCapacity,h_off, p)
   plant_animal_mat <- matrix(1, nrow = 17, ncol = 17)
-  plant_animal_mat[1,12:17] <- sigmoid(0.0025, 2600, agouti_vec[(i+1)])
+  plant_animal_mat[1,12:17] <- sigmoid(agouti_to_PlantSteepness, agoutiCapacity/2, agouti_vec[(i+1)]) # k was 0.0025
   plant_mat <- matrix( c((plant_animal_mat * pmat) %*% plant_mat))
   
   #Summing the stages into 3 categories for better plotting
@@ -118,10 +127,10 @@ for (i in 1:time_end)
 ### Plotting
 ###===========================================================================
 par(mar=c(5,4,1,1),oma=c(0,0,0,0))
-plot(agouti_vec/5200,xlab="",ylab="Population size/Max size", col="brown", ylim=c(0,1.5), type="l",xlim=c(1,time_end),xaxs="i")
-lines(plant_all[1,]/5000, col="forestgreen")
-lines(plant_all[2,]/500, col="turquoise3")
-lines(plant_all[3,]/100, col="orange")
+plot(agouti_vec/agoutiCapacity,xlab="",ylab="Population size/Max size", col="brown", ylim=c(0,1.5), type="l",xlim=c(1,time_end),xaxs="i")
+lines(plant_all[1,]/seedlingCapacity, col="forestgreen")
+lines(plant_all[2,]/saplingCapacity, col="turquoise3")
+lines(plant_all[3,]/adultCapacity, col="orange")
 legend("bottomleft", c("Animal","Adult","Sapling","Seedling"),col=c("brown","orange","turquoise3","forestgreen"),lty=c(1,1,1,1), bty="n",ncol=2)
 mtext("Time step", 1, line=1.85, at=25, col="black")
 axis(1,1:time_end,labels=toupper( substr(harvest_seq,1,1) ),line=2,col=NA,col.ticks=NA,col.axis="black", cex.axis=0.65)
