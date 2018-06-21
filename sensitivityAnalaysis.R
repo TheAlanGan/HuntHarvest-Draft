7a4b574617f48235d4d19436342e70d3cd2e521b
Eeman Abbasi <eabbasi@utk.edu>
2018-06-20 23:39:34 -0400

    Further revisions to the sensitivity analysis Cleared a bug in the previous code

--- sensitivityAnalaysis.R
+++ sensitivityAnalaysis.R
@@ -36,10 +36,11 @@ agouti_to_PlantSteepness <- -(log(1-m)-log(m))/((m-0.5)*agoutiCapacity) # Steepn
 plant_to_AgoutiSteepness <- -(log(1-m)-log(m))/((m-0.5)*adultCapacity)  # Steepness needed for sigmoid(m) = m
 #This formula above is derived from logistic function with "x = m*CAP" , "x0 = .5*CAP" , "y = m" , and solving for k. (CAP = carrying capacity)
 
-time_end <- 5000 # Length of simulation in years
+time_end <- 1000 # Length of simulation in years
 
 maxt <- 1000
 brazilNut <- list(low=plant_mat_low, high=plant_mat_high)
+
 high_harv <- matrix(1, nrow = 17, ncol = 17)
 
 
@@ -48,6 +49,11 @@ plant_S_mat <- matrix( 0, nrow = 17, ncol = 17)
 diag(plant_S_mat) <- c(0.455, 0.587, 0.78, 0.821, 0.941, 0.938, 0.961, 0.946, 0.94, 0.937, 0.936, 0.966, 0.968, 0.971, 0.965, 0.967, 0.985)
 plant_S_mat[cbind(2:17,1:16)] <- matrix(c(0.091, 0.147, 0.134, 0.167, 0.044, 0.047, 0.034, 0.049, 0.055, 0.058, 0.059, 0.029, 0.027, 0.024, 0.020, 0.018))
 plant_S_mat[1,12:17] <- matrix( c(12.3, 14.6, 16.9, 19.3, 22.3, 26.6) )
+high_harv[1,12:17] <- highHarvestFecundity 
+high_harv[cbind(12:17,12:17)] <- highHarvestSurvival # Multiplier for survival rate of Adult trees
+plant_mat_low <- plant_S_mat
+plant_mat_high <- plant_S_mat * high_harv
+
 
 
 #===========================================================================
@@ -208,21 +214,21 @@ plant_abundance_underHighHunt<- function(highHunting){
 
 #===================================================================================================================
 
-growthRate_mat<-matrix(0,101,101)
-#binary_mat<- matrix(0,21,21)
+growthRate_mat<-matrix(0,21,21)
+binary_mat<- matrix(0,21,21)
 rownames(growthRate_mat) <- paste(seq(0,1,0.01))
 colnames(growthRate_mat) <- paste(seq(0,1,0.01))
 
-#rownames(binary_mat) <- paste(seq(0,1,0.05))
-#colnames(binary_mat) <- paste(seq(0,1,0.05))
+rownames(binary_mat) <- paste(seq(0,1,0.05))
+colnames(binary_mat) <- paste(seq(0,1,0.05))
 
 num<-1 
 num1<-1
-for(i in seq(0,1,0.01))
+for(i in seq(0,1,0.05))
 {
   high_harv[1,12:17] <- i # Multiplier for fecundity rate for Adult trees
   num1<-1
-  for(j in seq(0,1,0.01)){
+  for(j in seq(0,1,0.05)){
 
     high_harv[cbind(12:17,12:17)] <- j # Multiplier for survival rate of Adult trees
     plant_mat_low <- plant_S_mat
@@ -230,13 +236,13 @@ for(i in seq(0,1,0.01))
     growth_rate <- exp(stoch_growth())
     growthRate_mat[num,num1]<-growth_rate
     print(growthRate_mat[num,num1])
-    #if(growthRate_mat[num,num1]>=1)
-    #{
-     # binary_mat[num,num1]<-1
-    #}
-    #else{
-     # binary_mat[num,num1]<-0
-    #}
+    if(growthRate_mat[num,num1]>=1)
+    {
+      binary_mat[num,num1]<-1
+    }
+    else{
+     binary_mat[num,num1]<-0
+    }
    
     num1<-num1+1
   }
@@ -244,16 +250,13 @@ for(i in seq(0,1,0.01))
   
 }
 
-#growthRate_heatmap <- heatmaply::heatmaply(binary_mat, Rowv=NA, Colv=NA, col = c("blue", "orange"), scale="none", margins=c(4,4), xlab = "Adult Survival", ylab="Germination")
 heatmaply::heatmaply(binary_mat,margins=c(4,4), Rowv=NA, Colv=NA,xlab = "Adult Survival", ylab="Germination")
 heatmaply::heatmaply(growthRate_mat,margins = c(4,4), Rowv=NA, Colv=NA,xlab = "Adult Survival", ylab="Germination", scale="none")
 
-
+#saving these the heatmap as images 
 dir.create("heatMaps")
 library(heatmaply)
-# Before the first time using this code you may need to first run:
-# webshot::install_phantomjs()
-heatmaply(binary_mat, file = "heatMaps/heatmaply_plot.png",Rowv=NA, Colv=NA,xlab = "Adult Survival", ylab="Germination")
+heatmaply(growthRate_mat, file = "heatMaps/heatmaply_plot.png",Rowv=NA, Colv=NA,xlab = "Adult Survival", ylab="Germination")
 browseURL("heatMaps/heatmaply_plot.png")
 
 #=====================================================================================================================
@@ -270,7 +273,7 @@ for(i in seq(0,1,0.05))
     num=num+1
 }
 
-plot(agouti_Growth,agouti_pop, xlab="Growth Rate", ylab="Animal Population", col="brown", ylim=c(0,4000), type="l",xlim=c(0,1.1),xaxs="i") 
+plot(agouti_Growth,agouti_pop, xlab="Proportion of the Growth Rate", ylab="Animal Population", col="brown", ylim=c(0,4000), type="l",xlim=c(0,1),xaxs="i") 
 #=======================================================================================================================
 #Carrying Capacity vs Agouti Population
 #=======================================================================================================================
@@ -285,7 +288,7 @@ for(i in seq(0,1,0.05))
   num<-num+1
 }
 
-plot(agouti_capacity,agouti_pop, xlab="Carrying Capacity", ylab="Animal Population", col="brown", ylim=c(2000,4300), type="l",xlim=c(0.55,1),xaxs="i")
+plot(agouti_capacity,agouti_pop, xlab="Proportion of the Carrying Capacity", ylab="Animal Population", col="brown", ylim=c(2000,4300), type="l",xlim=c(0.59,1),xaxs="i")
 
 
 #=========================================================================================================================
@@ -303,12 +306,11 @@ for(i in seq(0,1,0.05))
     seedling_hunt_mat[num]<- plant_abundance_underHighHunt(hunting_mat[num])[1]
     sapling_hunt_mat[num]<- plant_abundance_underHighHunt(hunting_mat[num])[2]
     adults_hunt_mat[num]<- plant_abundance_underHighHunt(hunting_mat[num])[3]
-    print(c(hunting_mat[num], plant_hunt_mat[num]))
     num= num+1
   }
 
 par(mar=c(5,4,1,1),oma=c(0,0,0,0))
-plot(hunting_mat, log(seedling_hunt_mat), xlab="Proportion of Animal Hunted", ylab="log(Plant Population)", col="brown", ylim=c(0,12), type="l",xlim=c(0.6,1),xaxs="i")
+plot(hunting_mat, log(seedling_hunt_mat), xlab="Proportion of Animal Hunted", ylab="log(Plant Population)", col="brown", ylim=c(0,35), type="l",xlim=c(0,1),xaxs="i")
 lines(hunting_mat,log(sapling_hunt_mat), col="black")
 lines(hunting_mat,log(adults_hunt_mat), col="green")
 legend(0.1, 5, legend=c("Seedling","Sapling","Adults"),col=c("brown", "black","green"), lty=1:2, cex=0.5)
@@ -318,8 +320,8 @@ legend(0.1, 5, legend=c("Seedling","Sapling","Adults"),col=c("brown", "black","g
 install.packages("akima")
 library(akima)
 
-filled.contour(x = seq(0,1,0.01),
-              y = seq(0,1,0.01),
+filled.contour(x = seq(0,1,0.05),
+              y = seq(0,1,0.05),
               z = growthRate_mat,
               color.palette =
                 colorRampPalette(c("white", "blue")),
