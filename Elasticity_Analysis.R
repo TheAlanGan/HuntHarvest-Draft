b7dcb91bf0602b71ac6f99c081497f87c72b97d9
Kevin De Angeli <kevindeangeli@hotmail.es>
2018-06-28 09:55:54 -0400

    I added code to vary high/low harvest and agouti population to observe the group bar chart

--- Elasticity_Analysis.R
+++ Elasticity_Analysis.R
@@ -224,39 +224,39 @@ plant_all <- matrix( c(seedlingInit, saplingInit, adultInit) ) # This will conta
 
 plot_var <- 1 #Controls how many plots will be produced
 plot_var2 <- time_end/3 #we can add 1000/2 to plot_var two times to get one plot in the middle and one at the end.
-                        #This variable is used below in an "if" statement. 
+                        #This variable is used below in an "if" statement.
 
-for (i in 1:time_end) 
-{
-  h_i <- harvest_seq[i]
-  
-  if (h_i == "low") 
-  {
-    pmat <- plant_mat_low
-    h_off <- lowHunting
-  } 
-  
-  else 
-  {
-    pmat <- plant_mat_high
-    h_off <- highHunting
-  }
-  
-  p <- sigmoid(plant_to_AgoutiSteepness, 50, sum(plant_mat[12:17]))*.1 + 0.9 # bounded between 0.9 and 1.0.... k was 0.1
-  agouti_vec[(i+1)] <- LogisticGrowthHunt(agoutiGrowth, agouti_vec[(i)],agoutiCapacity,h_off, p)
-    
-  
-  plant_animal_mat <- matrix(1, nrow = 17, ncol = 17)
-  plant_animal_mat[1,12:17] <- sigmoid(agouti_to_PlantSteepness, agoutiCapacity/2, agouti_vec[(i+1)]) # k was 0.0025
-  #  plant_animal_mat[1,12:17] <- linear(m, agouti_vec[(i+1)], b) # A different functional form
-  plant_mat <- matrix( c((plant_animal_mat * pmat) %*% plant_mat))
-  
-  #Summing the stages into 3 categories for better plotting
-  plant_mat_sum <- c( sum(plant_mat[1:4]), sum(plant_mat[5:11]), sum(plant_mat[12:17])) 
-  plant_all <- cbind(plant_all, plant_mat_sum)
-  
+# for (i in 1:time_end)
+# {
+#   h_i <- harvest_seq[i]
+# 
+#   if (h_i == "low")
+#   {
+#     pmat <- plant_mat_low
+#     h_off <- lowHunting
+#   }
+# 
+#   else
+#   {
+#     pmat <- plant_mat_high
+#     h_off <- highHunting
+#   }
+# 
+#   p <- sigmoid(plant_to_AgoutiSteepness, 50, sum(plant_mat[12:17]))*.1 + 0.9 # bounded between 0.9 and 1.0.... k was 0.1
+#   agouti_vec[(i+1)] <- LogisticGrowthHunt(agoutiGrowth, agouti_vec[(i)],agoutiCapacity,h_off, p)
+# 
+# 
+#   plant_animal_mat <- matrix(1, nrow = 17, ncol = 17)
+#   plant_animal_mat[1,12:17] <- sigmoid(agouti_to_PlantSteepness, agoutiCapacity/2, agouti_vec[(i+1)]) # k was 0.0025
+#   #  plant_animal_mat[1,12:17] <- linear(m, agouti_vec[(i+1)], b) # A different functional form
+#   plant_mat <- matrix( c((plant_animal_mat * pmat) %*% plant_mat))
+# 
+#   #Summing the stages into 3 categories for better plotting
+#   plant_mat_sum <- c( sum(plant_mat[1:4]), sum(plant_mat[5:11]), sum(plant_mat[12:17]))
+#   plant_all <- cbind(plant_all, plant_mat_sum)
+#   
 #-----------------------------------------------------------------------------
-#Elasticity stages
+#Elasticity by stages
 #-----------------------------------------------------------------------------  
   # avg_survival_seedling_elas <- c()
   # avg_growth_seedling_elas <- c()
@@ -265,9 +265,9 @@ for (i in 1:time_end)
   # avg_survival_adult_elas <- c()
   # avg_growth_adult_elas <- c()
   # avg_germination_adult_elas <-c()
-  
+
 dynamic_transition_matrix <- plant_animal_mat * pmat
-elasticity_matrix <- sensitivity_matrix(dynamic_transition_matrix)$elas 
+elasticity_matrix <- sensitivity_matrix(dynamic_transition_matrix)$elas
 #elasticity_matrix <-  plant_S_mat Just used to check if the values that come next match. 
 #Seedilging
 avg_survival_seedling_elas[i] <- sum(diag(elasticity_matrix[1:4,1:4]))/4  
@@ -293,14 +293,61 @@ surv_elas_vec[i] <- sum(diag(elasticity_matrix)) #Elasticity for survival
 germination_elas_vec[i] <-sum(elasticity_matrix[1,12:17]) #Elasticity for germination
   
 
-}
+#}
 
-##ggplot. 
-#Hypercube latinHypercube. 
+
 #elasticity_contineous_graph(avg_survival_adult_elas,avg_growth_adult_elas,avg_germination_adult_elas)
 
 #dynamic_transition_matrix <- plant_animal_mat * pmat
 #elasticity_demographic_process_bar_plot(dynamic_transition_matrix)
+
+
+#----------------------------------------------------------------------------
+#Grouped_bar_code + Example use
+#----------------------------------------------------------------------------
+agouti_pop <- 0
+plant_animal_mat <- matrix(1, nrow = 17, ncol = 17)
+plant_animal_mat[1,12:17] <- sigmoid(agouti_to_PlantSteepness, agoutiCapacity/2, agouti_pop) # k was 0.0025
+plant_matrixLowHigh <- plant_mat_high
+dynamic_transition_matrix <- plant_animal_mat * plant_matrixLowHigh
+
+plants_matrix_name <- "Plant_mat_low"
+if(plant_matrixLowHigh[17,17]==plant_mat_high[17,17])
+{
+  plants_matrix_name <- "Plant_mat_high"
+}
+
+elasticity_matrix <- sensitivity_matrix(dynamic_transition_matrix)$elas 
+
+avg_survival_seedling_elas2 <- sum(diag(elasticity_matrix[1:4,1:4]))/4  
+avg_growth_seedling_elas2 <- sum(diag(elasticity_matrix[2:5,1:4]))/4 
+#Sapling
+avg_survival_sapling_elas2 <- sum(diag(elasticity_matrix[5:11,5:11]))/7
+avg_growth_sapling_elas2 <- sum(diag(elasticity_matrix[6:12,5:11]))/7
+#Adult
+avg_survival_adult_elas2 <- sum(diag(elasticity_matrix[12:17,12:17]))/6
+avg_growth_adult_elas2 <- sum(diag(elasticity_matrix[13:17,12:16]))/5
+avg_germination_adult_elas2 <- sum(diag(elasticity_matrix[1,12:17]))/6
+
+#Function call (it accepts 7 arguments) -------------------------------------------
+
+grouped_bar_code <- function(avg_survival_seedling_elas2, avg_growth_seedling_elas2,avg_survival_sapling_elas2,avg_growth_sapling_elas2,avg_survival_adult_elas2,avg_growth_adult_elas2,avg_germination_adult_elas2)
+{
+  library(ggplot2)
+  
+  # create a dataset
+  stage=c(rep("Seedling" , 3) , rep("Sapling" , 3) , rep("Adult" , 3))
+  demographic_process=rep(c("Survival" , "Growth" , "Germination") , 3)
+  elasticity=(c(avg_survival_seedling_elas2, avg_growth_seedling_elas2,0,avg_survival_sapling_elas2,avg_growth_sapling_elas2,0,avg_survival_adult_elas2,avg_growth_adult_elas2,avg_germination_adult_elas2)) #Put the values here
+  data=data.frame(stage,demographic_process,elasticity)
+  
+  # Grouped
+  ggplot(data, aes(fill=demographic_process, y=elasticity, x=stage)) +
+    geom_bar(position="dodge", stat="identity") + ggtitle(paste("Agouti: ",agouti_pop, "  -  ", plants_matrix_name)) +ylim(0,0.15)
+  
+}
+
+grouped_bar_code(avg_survival_seedling_elas2, avg_growth_seedling_elas2,avg_survival_sapling_elas2,avg_growth_sapling_elas2,avg_survival_adult_elas2,avg_growth_adult_elas2,avg_germination_adult_elas2)
 
 
 
